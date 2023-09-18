ca5ccb9a4c8b1257e4b923fe651022d66d1a4303
only-zover <j0seextremehd@outlook.com>
2023-09-18 17:19:19 -0300

    base template+

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const express = require("express")
-const app = express()
+const express = require('express');
+const path = require('path');
 
-const PORT = process.env.PORT || 8080
+const indexRouter = require('./routes/index');
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`)
-})
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
