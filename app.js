5140904f735e1cb513d1ea672ab083449871448e
only-zover <j0seextremehd@outlook.com>
2023-09-18 16:58:37 -0300

    rename index to app

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`)
+    console.log(`Server is running on port ${PORT}.`)
 })
