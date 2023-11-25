119e3a54b07d85bfe79c5de62dce88d30f0bcec6
Thales <thalesondas0@gmail.com>
2023-11-25 17:42:08 -0300

    buscar lista de meninas qnd mostrar a página e não com botão

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,39 @@
 $(function() {
 	'use strict';
 
+	//função buscarMeninas para a tela de Selecao
+	async function buscarMeninas() {
+		try {
+			const response = await fetch('/meninas');
+			const meninas = await response.json();
+
+			const tableBody = document.getElementById('table-body');
+			tableBody.innerHTML = ''; // Limpa a tabela para sobreescrever
+
+			// Itera sobre os dados obtidos e os adiciona à tabela
+			meninas.forEach((menina) => {
+				const row = document.createElement('tr');
+				row.innerHTML = `
+					<td>${menina.nome}</td>
+					<td>${menina.idade}</td>
+					<td>${menina.cidade} - ${menina.estado}</td>
+					<td>R$ ${menina.renda}</td>
+				`;
+				tableBody.appendChild(row);
+			});
+		} catch (error) {
+			console.error('Erro ao obter meninas:', error);
+		}
+	}
+
 	// Colocar a página home na main quando entrar no site
 	$(document).ready(function () {
 		$('main').load('views/home.handlebars');
 		
 		$('#home').addClass('active');
 	});
+
+	
   
 	// Função para destacar a página em que o usuário escolha entrar e mudar o conteúdo
 	$('.nav-menu li').click(function(e) {
@@ -22,7 +49,9 @@ $(function() {
 	  var target = $(this).attr('id');
   
 	  // Carregue o conteúdo da página desejada
-	  $('main').load('views/' + target + '.handlebars');
+	  $('main').load('views/' + target + '.handlebars', function() {
+		buscarMeninas();
+	  });
 	});
   
 	 // Mostrar e esconder a sidebar pelo menu-toggle
